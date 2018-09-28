5f74d6eb3fef3a1f2687f8f514b5f9c3f4809774
quinclass <43576119+quinclass@users.noreply.github.com>
2018-09-28 11:45:51 +0200

    280918

--- - html css js -/calculator.js
+++ - html css js -/calculator.js
@@ -11,21 +11,110 @@ document.onreadystatechange = function () {
     }
 }
 
+function init(){}
 
 var dot = false;
-var sign = "";
+var _operator = "";
+var _memory = "";
 
-function operator( __o ){
-    switch(__o){
-        case '.':
+function operator_dot(){
             if (dot==false && document.getElementById("screen").innerText!=""){
                 document.getElementById("screen").innerText+=".";
                 dot=true;
             }
-            break;
-        case 'c':
+        }
+        function operator_clear(){
             document.getElementById("screen").innerText=""
-            dot=false;
-            break;
-    }
+            dot = false;
+            _operator = "";
+            _memory = "";
+        }
+        
+        function operator_plus(){
+            if(document.getElementById("screen").innerText===""){
+                operator_clean();
+                return;
+            }
+            _memory = document.getElementById("screen").innerText;
+            _operator = "+";
+            document.getElementById("screen").innerText="";
+        }
+
+        function operator_minus(){
+            if(document.getElementById("screen").innerText===""){
+                operator_clean();
+                return;
+            }
+            _memory = document.getElementById("screen").innerText;
+            _operator = "-";
+            document.getElementById("screen").innerText="";
+        }
+
+        function operator_mul(){
+            if(document.getElementById("screen").innerText===""){
+                operator_clean();
+                return;
+            }
+            _memory = document.getElementById("screen").innerText;
+            _operator = "x";
+            document.getElementById("screen").innerText="";
+        }
+
+        function operator_divide(){
+            if(document.getElementById("screen").innerText===""){
+                operator_clean();
+                return;
+            }
+            _memory = document.getElementById("screen").innerText;
+            _operator = ":";
+            document.getElementById("screen").innerText="";
+           }
+
+           function operator_equal(){
+            if(document.getElementById("screen").innerText===""){
+                operator_clean();
+                return;
+            }
+
+            let _temp = document.getElementById("screen").innerText;
+            if(_operator=="+"){
+                document.getElementById("screen").innerText=""+                
+                    (parseFloat(document.getElementById("screen").innerText)+parseFloat(_memory));
+            }
+            if(_operator=="-"){
+                document.getElementById("screen").innerText=""+                
+                    (parseFloat(document.getElementById("screen").innerText)-parseFloat(_memory));
+            }
+            if(_operator=="x"){
+                document.getElementById("screen").innerText=""+                
+                    (parseFloat(document.getElementById("screen").innerText)*parseFloat(_memory));
+            }
+            if(_operator==":"){
+                document.getElementById("screen").innerText=""+                
+                    (parseFloat(document.getElementById("screen").innerText)/parseFloat(_memory));
+            }
+
+            pushtodb(_memory,_temp,_operator,document.getElementById("screen").innerText);
+        }         
+    
+function pushtobd(st, nd, op, res){
+    const _data = {
+        first: st,
+        second: nd,
+        operator: op,
+        result: res
+    };
+    const _header = new Headers();
+    _header.append("Content-Type", "application/json");
+
+    const _options = {
+        method: "POST",
+        body: JSON.stringify(_data),
+        headers: _header
+    };
+
+    const _request = new Request("http://localhost:3000/results", _options);
+    fetch(_request)
+        .then( function(){ alert("data pushed to db"); })
+        .catch( function(){ alert("server unreachable") });
 }
